cdeddaf252481a11c755ab68d7f64ed3651e020b
Seebs <seebs@sourcegraph.com>
2018-06-11 16:34:26 -0500

    fix thing

--- t.sh
+++ t.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ls -a | xargs -n 3 -n=4 ls -alzx
+cp -alx foo bar
 tar czf
 git rev-list --all
 ls -- \
-	";" "|" '\' rm -r
-cp -alx
+	";" "|" '\' rm -f
+rm -f
 ls -- \
 	";" "|" '\
-' cp -alx
+' rm -f
 rm -f
 ls -a
